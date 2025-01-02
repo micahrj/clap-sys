19157867038b4aa4fdde54abee7b9894ab651b77
glowcoil <micahrjohnston@gmail.com>
2025-01-02 17:10:17 -0600

    update for CLAP 1.2.2

--- src/ext/draft/undo.rs
+++ src/ext/draft/undo.rs
@@ -2,27 +2,21 @@ use crate::{cstr, host::*, id::*, plugin::*};
 
 use std::ffi::{c_char, c_void, CStr};
 
-pub const CLAP_EXT_UNDO: &CStr = cstr!("clap.undo/2");
-
-pub const CLAP_UNDO_IS_WITHIN_CHANGE: clap_undo_context_flags = 1 << 0;
-
-pub type clap_undo_context_flags = u64;
-
-pub const CLAP_UNDO_DELTA_PROPERTIES_HAS_DELTA: clap_undo_delta_properties_flags = 1 << 0;
-pub const CLAP_UNDO_DELTA_PROPERTIES_IS_PERSISTENT: clap_undo_delta_properties_flags = 1 << 1;
-
-pub type clap_undo_delta_properties_flags = u64;
+pub const CLAP_EXT_UNDO: &CStr = cstr!("clap.undo/4");
+pub const CLAP_EXT_UNDO_CONTEXT: &CStr = cstr!("clap.undo_context/4");
+pub const CLAP_EXT_UNDO_DELTA: &CStr = cstr!("clap.undo_delta/4");
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct clap_undo_delta_properties {
-    pub flags: clap_undo_delta_properties_flags,
-    pub format_version: u32,
+    pub has_delta: bool,
+    pub are_deltas_persistent: bool,
+    pub format_version: clap_id,
 }
 
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-pub struct clap_plugin_undo {
+pub struct clap_plugin_undo_delta {
     pub get_delta_properties: Option<
         unsafe extern "C" fn(
             plugin: *const clap_plugin,
@@ -31,7 +25,7 @@ pub struct clap_plugin_undo {
     >,
     pub can_use_delta_format_version:
         Option<unsafe extern "C" fn(plugin: *const clap_plugin, format_version: clap_id) -> bool>,
-    pub apply_delta: Option<
+    pub undo: Option<
         unsafe extern "C" fn(
             plugin: *const clap_plugin,
             format_version: clap_id,
@@ -39,14 +33,25 @@ pub struct clap_plugin_undo {
             delta_size: usize,
         ) -> bool,
     >,
-    pub set_context_info: Option<
+    pub redo: Option<
         unsafe extern "C" fn(
             plugin: *const clap_plugin,
-            flags: clap_undo_context_flags,
-            undo_name: *const c_char,
-            redo_name: *const c_char,
-        ),
+            format_version: clap_id,
+            delta: *const c_void,
+            delta_size: usize,
+        ) -> bool,
     >,
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct clap_plugin_undo_context {
+    pub set_can_undo: Option<unsafe extern "C" fn(plugin: *const clap_plugin, can_undo: bool)>,
+    pub set_can_redo: Option<unsafe extern "C" fn(plugin: *const clap_plugin, can_redo: bool)>,
+    pub set_undo_name:
+        Option<unsafe extern "C" fn(plugin: *const clap_plugin, name: *const c_char)>,
+    pub set_redo_name:
+        Option<unsafe extern "C" fn(plugin: *const clap_plugin, name: *const c_char)>,
 }
 
 #[repr(C)]
@@ -58,14 +63,13 @@ pub struct clap_host_undo {
         unsafe extern "C" fn(
             host: *const clap_host,
             name: *const c_char,
-            redo_delta: *const c_void,
-            redo_delta_size: usize,
-            undo_delta: *const c_void,
-            undo_delta_size: usize,
+            delta: *const c_void,
+            delta_size: usize,
+            delta_can_undo: bool,
         ),
     >,
-    pub undo: Option<unsafe extern "C" fn(host: *const clap_host)>,
-    pub redo: Option<unsafe extern "C" fn(host: *const clap_host)>,
-    pub set_context_info_subscription:
+    pub request_undo: Option<unsafe extern "C" fn(host: *const clap_host)>,
+    pub request_redo: Option<unsafe extern "C" fn(host: *const clap_host)>,
+    pub set_wants_context_updates:
         Option<unsafe extern "C" fn(host: *const clap_host, is_subscribed: bool)>,
 }
