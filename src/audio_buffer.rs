adc8b59d664a1879c67eea86895732b237af0e26
glowcoil <micahrjohnston@gmail.com>
2025-01-02 13:56:24 -0600

    clap_audio_buffer data32/data64 pointers should be *mut

--- src/audio_buffer.rs
+++ src/audio_buffer.rs
@@ -1,8 +1,8 @@
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct clap_audio_buffer {
-    pub data32: *const *const f32,
-    pub data64: *const *const f64,
+    pub data32: *mut *mut f32,
+    pub data64: *mut *mut f64,
     pub channel_count: u32,
     pub latency: u32,
     pub constant_mask: u64,
