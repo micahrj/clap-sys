be2e79484056ccac5e816a0dc44b01166a52399f
Micah Johnston <micahrjohnston@gmail.com>
2026-07-20 11:15:25 -0500

    type aliases for clap_project_location_kind, clap_project_location_track_kind, and clap_project_location_flags

--- src/ext/draft/project_location.rs
+++ src/ext/draft/project_location.rs
@@ -9,27 +9,33 @@ use core::ffi::{c_char, CStr};
 
 pub const CLAP_EXT_PROJECT_LOCATION: &CStr = cstr!("clap.project-location/2");
 
-pub const CLAP_PROJECT_LOCATION_PROJECT: u32 = 1;
-pub const CLAP_PROJECT_LOCATION_TRACK_GROUP: u32 = 2;
-pub const CLAP_PROJECT_LOCATION_TRACK: u32 = 3;
-pub const CLAP_PROJECT_LOCATION_DEVICE: u32 = 4;
-pub const CLAP_PROJECT_LOCATION_NESTED_DEVICE_CHAIN: u32 = 5;
+pub const CLAP_PROJECT_LOCATION_PROJECT: clap_project_location_kind = 1;
+pub const CLAP_PROJECT_LOCATION_TRACK_GROUP: clap_project_location_kind = 2;
+pub const CLAP_PROJECT_LOCATION_TRACK: clap_project_location_kind = 3;
+pub const CLAP_PROJECT_LOCATION_DEVICE: clap_project_location_kind = 4;
+pub const CLAP_PROJECT_LOCATION_NESTED_DEVICE_CHAIN: clap_project_location_kind = 5;
 
-pub const CLAP_PROJECT_LOCATION_INSTRUMENT_TRACK: u32 = 1;
-pub const CLAP_PROJECT_LOCATION_AUDIO_TRACK: u32 = 2;
-pub const CLAP_PROJECT_LOCATION_HYBRID_TRACK: u32 = 3;
-pub const CLAP_PROJECT_LOCATION_RETURN_TRACK: u32 = 4;
-pub const CLAP_PROJECT_LOCATION_MASTER_TRACK: u32 = 5;
+pub type clap_project_location_kind = u32;
 
-pub const CLAP_PROJECT_LOCATION_HAS_INDEX: u64 = 1 << 0;
-pub const CLAP_PROJECT_LOCATION_HAS_COLOR: u64 = 1 << 1;
+pub const CLAP_PROJECT_LOCATION_INSTRUMENT_TRACK: clap_project_location_track_kind = 1;
+pub const CLAP_PROJECT_LOCATION_AUDIO_TRACK: clap_project_location_track_kind = 2;
+pub const CLAP_PROJECT_LOCATION_HYBRID_TRACK: clap_project_location_track_kind = 3;
+pub const CLAP_PROJECT_LOCATION_RETURN_TRACK: clap_project_location_track_kind = 4;
+pub const CLAP_PROJECT_LOCATION_MASTER_TRACK: clap_project_location_track_kind = 5;
+
+pub type clap_project_location_track_kind = u32;
+
+pub const CLAP_PROJECT_LOCATION_HAS_INDEX: clap_project_location_flags = 1 << 0;
+pub const CLAP_PROJECT_LOCATION_HAS_COLOR: clap_project_location_flags = 1 << 1;
+
+pub type clap_project_location_flags = u64;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
 pub struct clap_project_location_element {
-    pub flags: u64,
-    pub kind: u32,
-    pub track_kind: u32,
+    pub flags: clap_project_location_flags,
+    pub kind: clap_project_location_kind,
+    pub track_kind: clap_project_location_track_kind,
     pub index: u32,
     pub id: [c_char; CLAP_PATH_SIZE],
     pub name: [c_char; CLAP_NAME_SIZE],
